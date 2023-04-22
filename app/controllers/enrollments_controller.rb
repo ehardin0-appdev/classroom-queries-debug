2395f7e6ff8b4453991a24f2fd4a7add8713e290
Elle Harding <ehardin0@chicagobooth.edu>
2023-04-22 00:26:30 +0000

    Commit

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -10,7 +10,7 @@ class EnrollmentsController < ApplicationController
   def show
     the_id = params.fetch("path_id")
 
-    matching_enrollments = Enrollment.where({ :id => the_id })
+    @matching_enrollments = Enrollment.where({ :id => the_id })
 
     @the_enrollment = matching_enrollments.at(0)
 
